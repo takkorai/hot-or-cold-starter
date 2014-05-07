c752537f14bf927691301faeaa3595c06253c82b
takkorai <takkorai@gmail.com>
2014-05-07 07:46:10 -0400

    added variables

--- js/app.js
+++ js/app.js
@@ -11,6 +11,31 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  	/*
+  	var randNumber = function(num){
+  		var num=Math.floor((Math.random() * 100) + 1);
+
+  	};
+
+  	var guessGame=function(enteredNum,randNumber){
+  		if(enteredNum>randNumber){
+  			var answer = "Higher";
+  		};
+  		else if(enteredNum<randNumber){
+  			answer="lower";
+  		};
+  		else if{enteredNum=randNumber}{
+  			answer="Same";
+  		};
+  	};*/
+
+  	$(".button").click(function(event){
+  		console.log("clicked");
+  		event.PreventDefault();
+  		var enteredNum=$(".userGuess").val();
+  		console.log(enteredNum);
+  	
+  	});
 
 });
 
